719ca46b197729c3f1057bf4276f9c89eca13105
Matthew Hallberg <matthewhallberg@gmail.com>
2020-03-21 17:06:54 -0400

    first commit, URP working with ARFoundation.

---
+++ Assets/Scripts/API.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class API : MonoBehaviour {
+
+    const string ENDPOINT = "http://coronavirusapi.com/states.csv";
+
+    void Start() {
+        GetVirusData();
+    }
+
+    void GetVirusData() {
+        StartCoroutine(GetVirusDataRoutine());
+    }
+
+    IEnumerator GetVirusDataRoutine() {
+        UnityWebRequest webRequest = UnityWebRequest.Get(ENDPOINT);
+        // Request and wait for the desired page.
+        yield return webRequest.SendWebRequest();
+
+        if (webRequest.isNetworkError) {
+            Debug.Log("Error: " + webRequest.error);
+        } else {
+            Debug.Log("Received: " + webRequest.downloadHandler.text);
+        }
+    }
+}
